6d1c6dd11b024bc9bdbfbaecc960b4ae18fc84f3
Denys Plietnikov <givisimo@gmail.com>
2019-07-04 00:32:31 +0300

    console error fix

--- src/index.js
+++ src/index.js
@@ -8,24 +8,36 @@ const mainUl = document.querySelector('.menu');
 mainUl.insertAdjacentHTML('beforeend', resultA);
 const body = document.querySelector('body');
 const themeBtn = document.querySelector('button[data-action="theme-switch"]');
+const btnStyle = document.querySelector('.toolbar__icon');
 
 themeBtn.addEventListener('click', handleClick);
 checkTheme();
 
 function checkTheme() {
   if (localStorage.theme === 'dark') {
-    body.classList.add('theme-dark');
+    setDarkTheme();
   } else if (localStorage.theme === 'light') {
-    body.classList.add('theme-light');
+    setLightTheme();
   }
 }
 
 function handleClick() {
   if (body.classList.contains('theme-dark')) {
-    body.classList.replace('theme-dark', 'theme-light');
+    body.classList.remove('theme-dark');
     localStorage.setItem('theme', 'light');
+    setLightTheme();
   } else {
-    body.classList.add('theme-dark');
+    setDarkTheme();
     localStorage.setItem('theme', 'dark');
   }
+}
+
+function setLightTheme() {
+  body.classList.add('theme-light');
+  btnStyle.textContent = 'wb_sunny';
+}
+
+function setDarkTheme() {
+  body.classList.add('theme-dark');
+  btnStyle.textContent = 'brightness_3';
 }
